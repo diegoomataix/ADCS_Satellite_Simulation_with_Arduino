f3520306d9691a89eca30098f075a413cf1472e7
diegoomataix <diego.mataix@hotmail.com>
2021-12-23 22:21:09 +0100

    Updated code

--- 2.Arduino Scripts/1.GettingStarted/Test1/Test1.ino
+++ 2.Arduino Scripts/1.GettingStarted/Test1/Test1.ino
@@ -1,10 +1,12 @@
 //include the servo library:
 #include <Servo.h>
+
 //variables for storing values from LDRs:
 int ldr1;
 int ldr2;
 //variable for the servo angle, initial value 90:
 int angle = 90;
+
 //create a servo object:
 Servo myServo;
 
@@ -12,39 +14,47 @@ void setup()
 {
     myServo.attach(11);   //servo is connected to pin 11
     myServo.write(angle); // set servo to starting ange
-    Serial.begin(9600);   //start serial communication
+    Serial.begin(115200);   //start serial communication
 }
 
-void loop()
+void determination()
 {
     //read both sensor values and and store to variables:
     ldr1 = analogRead(A0);
     ldr2 = analogRead(A1);
+}
 
+void control()
+{
     //if value from ldr1 is bigger than ldr2,
     //decrease the angle by 2 degrees:
     if (ldr1 > ldr2)
     {
         angle = angle - 2;
-        //else increase the angle:
+    //else increase the angle:
     }
     else if (ldr1 <= ldr2)
     {
         angle = angle + 2;
     }
+
     //check if the angle has reached the maximum.
     //if it has, set angle to 180.
     if (angle > 180)
     {
         angle = 180;
     }
+
     //check if the angle has reached the minimum.
     //if it has, set angle to 0:
     if (angle < 0)
     {
         angle = 0;
     }
+}
 
+void display()
+{
     //print values for debugging purposes:
     Serial.print("ldr1: ");
     Serial.print(ldr1);
@@ -52,9 +62,20 @@ void loop()
     Serial.print(ldr2);
     Serial.print(" angle: ");
     Serial.println(angle);
+}
 
-    //finally, set the servo to an angle:
+void actuate()
+{
+    //write the angle to the servo:
     myServo.write(angle);
+}
+
+void loop()
+{
+    determination();
+    control();
+    display();
+    actuate();
     //a small delay:
-    delay(20);
+    delay(10);
 }
